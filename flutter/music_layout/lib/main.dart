2cb008f4f981240200b3dd87aacba7e31de58ddf
KangHuiSoo <org.woodie@gmail.com>
2024-02-20 16:29:43 +0900

    뮤직 레이아웃 퍼플 테마

--- flutter/music_layout/lib/main.dart
+++ flutter/music_layout/lib/main.dart
@@ -24,67 +24,78 @@ class MusicPlayerScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Music Player'),
+        backgroundColor: Colors.purpleAccent,
       ),
       body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [Colors.purple, Colors.purpleAccent],
+          ),
+        ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Expanded(
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      height: 200,
-                      width: 200,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          image: DecorationImage(
-                              image: AssetImage('assets/images/coding.gif'),
-                              fit: BoxFit.cover)),
-                    ),
-                    SizedBox(height: 10.0),
-                    Text(
-                      "Song Title",
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 10.0),
-                    Text(
-                      "Artist Name",
-                      style:
-                          TextStyle(fontSize: 18, fontStyle: FontStyle.italic),
-                    ),
-                  ],
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/images/coding.gif'),
+                  radius: 100,
                 ),
-              ),
+                // Container(
+                //   height: 200,
+                //   width: 200,
+                //   decoration: BoxDecoration(
+                //       borderRadius: BorderRadius.circular(10),
+                //       image: DecorationImage(
+                //           image: AssetImage('assets/images/coding.gif'),
+                //           fit: BoxFit.cover)),
+                // ),
+                SizedBox(height: 10.0),
+                Text(
+                  "Song Title",
+                  style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                SizedBox(height: 10.0),
+                Text(
+                  "Artist Name",
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.white70),
+                ),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-              child: Column(
-                children: [
-                  Slider(value: 0.1, onChanged: (value) {}),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      IconButton(
-                        onPressed: () {},
-                        icon: Icon(Icons.skip_previous),
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: Icon(Icons.play_arrow),
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: Icon(Icons.skip_next),
-                      )
-                    ],
-                  )
-                ],
-              ),
+            SizedBox(height: 30.0),
+            Slider(
+              value: 0.1,
+              onChanged: (value) {},
+              activeColor: Colors.white,
+              inactiveColor: Colors.grey,
+            ),
+            SizedBox(height: 10.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon:
+                      Icon(Icons.skip_previous, size: 50, color: Colors.white),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.play_arrow, size: 50, color: Colors.white),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.skip_next, size: 50, color: Colors.white),
+                )
+              ],
             )
           ],
         ),
