73f71c0496a76ee93e23e00d69edbb6d5077bd92
KangHuiSoo <org.woodie@gmail.com>
2024-02-22 14:26:40 +0900

    다중 Form, submit 처리

--- flutter/form/lib/main.dart
+++ flutter/form/lib/main.dart
@@ -14,6 +14,22 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   final _formKey = GlobalKey<FormState>();
   String _name = '';
+  String _email = '';
+  String _password = '';
+  String _errorMessage = '';
+
+  void submitForm() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      setState(() {
+        _errorMessage = '';
+      });
+    } else {
+      setState(() {
+        _errorMessage = 'Please fill all fields';
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,7 +57,40 @@ class _MyAppState extends State<MyApp> {
                     onSaved: (value) {
                       _name = value!;
                     },
-                  )
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: 'Email'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your email';
+                      }
+                      return null; // 유효성검사 통과시 null을 리턴
+                    },
+                    onSaved: (value) {
+                      _email = value!;
+                    },
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: 'Password'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your password';
+                      }
+                      return null; // 유효성검사 통과시 null을 리턴
+                    },
+                    onSaved: (value) {
+                      _password = value!;
+                    },
+                  ),
+                  SizedBox(height: 10.0),
+                  ElevatedButton(
+                    onPressed: submitForm,
+                    child: Text('Submit'),
+                  ),
+                  Text(
+                    '$_errorMessage',
+                    style: TextStyle(color: Colors.red),
+                  ),
                 ],
               )),
         ),
