4cbc6174f01dd22089b88ab228d1c3a6ffaa6730
KangHuiSoo <org.woodie@gmail.com>
2024-02-22 14:15:13 +0900

    TextFormField 기본

---
+++ flutter/form/android/app/src/main/kotlin/com/woodie/form/form/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.woodie.form.form
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter/form/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _formKey = GlobalKey<FormState>();
+  String _name = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Form Example'),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(16.0),
+          child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(labelText: 'Name'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your name';
+                      }
+                      return null; // 유효성검사 통과시 null을 리턴
+                    },
+                    onSaved: (value) {
+                      _name = value!;
+                    },
+                  )
+                ],
+              )),
+        ),
+      ),
+    );
+  }
+}
