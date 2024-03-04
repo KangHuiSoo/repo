1d3f6e2354b5b37839be5b17465c2da25ca48e9a
KangHuiSoo <org.woodie@gmail.com>
2024-03-04 20:03:23 +0900

    제스터디텍터 드래그

--- flutter/gesture_detector/lib/main.dart
+++ flutter/gesture_detector/lib/main.dart
@@ -10,11 +10,58 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
+      home: DragGesture(),
     );
   }
 }
 
+class DragGesture extends StatefulWidget {
+  const DragGesture({Key? key}) : super(key: key);
+
+  @override
+  State<DragGesture> createState() => _DragGestureState();
+}
+
+class _DragGestureState extends State<DragGesture> {
+  double _top = 0;
+  String _dragStatue = 'Drag the text vertical';
+
+  void _onVerticalDrageStart(DragStartDetails details) {
+    setState(() {
+      _dragStatue = 'Vertical drag started';
+      _top = details.globalPosition.dy;
+    });
+  }
+
+  void _onVerticalDragUpdate(DragUpdateDetails details){
+    setState(() {
+      _top += details.delta.dy;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Drag'),
+      ),
+      body: GestureDetector(
+        onVerticalDragStart: _onVerticalDrageStart,
+        onVerticalDragUpdate: _onVerticalDragUpdate,
+        child: Container(
+          child: Stack(
+            children: [
+              Positioned(
+                top: _top,
+                child: Text(_dragStatue),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -26,7 +73,7 @@ class MyHomePage extends StatelessWidget {
         title: Text("Gesture Demo"),
       ),
       body: GestureDetector(
-        onTap: (){
+        onTap: () {
           print('Screen tapped');
         },
         child: Container(
